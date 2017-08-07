db0fe0aeb11024d11ea5d95e9ecee242ba7ae787
adisak1618 <adisakchaiyakul@gmail.com>
2017-08-07 16:49:49 +0700

    add purecss, contact component with data set

--- app/components/Layout.js
+++ app/components/Layout.js
@@ -1,10 +1,24 @@
 import React from 'react';
 
+const data = [
+  { name: 'Adisak Chaiyakul', email: 'adisakchaiyakul@gmail.com' },
+  { name: 'Chachis Mathew', email: 'Mathew@gmail.com' },
+  { name: 'Jaramie Lucus', email: 'Jaramie@gmail.com' }
+];
+
+const Contact = props =>
+  <div className='pure-u-1-3'>
+    <h2>{props.name}</h2>
+    <p>{props.email}</p>
+  </div>;
+
 class Layout extends React.Component {
   render() {
     return (
-      <div id='helloWorld'>
-        Hello From Layout!
+      <div className='pure-g' >
+        {data.map(info =>
+          <Contact {...info} />
+        )}
       </div>
     );
   }
--- app/index.js
+++ app/index.js
@@ -1,8 +1,6 @@
+import 'purecss/build/pure.css';
 import React from 'react';
 import { render } from 'react-dom';
-
-import './style.css';
-
 import { Layout } from './components';
 
 render(<Layout />, document.getElementById('app'));
