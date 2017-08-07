6d0826342c0ce62fe07bea03ea6745fb0fb94c22
adisak1618 <adisakchaiyakul@gmail.com>
2017-08-07 11:50:42 +0700

    init code

---
+++ app/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { render } from 'react-dom';
+
+import './style.css';
+
+import HelloWorld from './components';
+
+render(<HelloWorld />, document.getElementById('app'));
