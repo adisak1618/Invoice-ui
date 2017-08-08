b49fed83cd4861a3e4976d3ab37eb1d1c22cde65
adisak1618 <adisakchaiyakul@gmail.com>
2017-08-08 13:53:52 +0700

    clean up our component

--- app/components/Layout.js
+++ app/components/Layout.js
@@ -1,24 +1,49 @@
 import React from 'react';
-
-const data = [
-  { name: 'Adisak Chaiyakul', email: 'adisakchaiyakul@gmail.com' },
-  { name: 'Chachis Mathew', email: 'Mathew@gmail.com' },
-  { name: 'Jaramie Lucus', email: 'Jaramie@gmail.com' }
-];
-
-const Contact = props =>
-  <div className='pure-u-1-3'>
-    <h2>{props.name}</h2>
-    <p>{props.email}</p>
-  </div>;
+import data from './data';
+import Contact from './Contact';
+import './Layout.css';
 
 class Layout extends React.Component {
+  componentWillMount() {
+    this.setState({
+      contacts: data,
+    });
+  }
+  addContact = (e) => {
+    e.preventDefault();
+    const contacts = this.state.contacts;
+    const newID = contacts[contacts.length - 1].id + 1;
+    this.setState({
+      contacts: contacts.concat({
+        id: newID,
+        name: `New Contact ${newID}`,
+        email: `${newID}@example.com`,
+      }),
+    });
+  }
+
+  newContact = () =>
+    <div className='pure-g'>
+      <div className='pure-u-12-24'>
+        <form className='pure-form'>
+          <fieldset>
+            <legend> New Contact </legend>
+            <input type='email' placeholder='example@example.com' />
+            <input type='text' placeholder='Name' />
+            <button type="submit" className='pure-button pure-button-primary' onClick={this.addContact}>Add</button>
+          </fieldset>
+        </form>
+      </div>
+    </div>;
   render() {
     return (
-      <div className='pure-g' >
-        {data.map(info =>
-          <Contact {...info} />
-        )}
+      <div id='Layout'>
+        {this.newContact()}
+        <div className='pure-g' >
+          {this.state.contacts.map(info =>
+            <Contact {...info} key={info.id}/>
+          )}
+        </div>
       </div>
     );
   }
---
+++ app/components/data.js
@@ -0,0 +1,7 @@
+const data = [
+  { id: 1, name: 'Adisak Chaiyakul', email: 'adisakchaiyakul@gmail.com' },
+  { id: 2, name: 'Chachis Mathew', email: 'Mathew@gmail.com' },
+  { id: 3, name: 'Jaramie Lucus', email: 'Jaramie@gmail.com' }
+];
+
+export default data;
