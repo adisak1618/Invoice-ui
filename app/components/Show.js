4e0211164999ec8fb67df21ec37d44cef0fff55e
adisak1618 <adisakchaiyakul@gmail.com>
2017-08-08 16:00:45 +0700

    add contact detail

--- app/components/Show.js
+++ app/components/Show.js
@@ -1,10 +1,18 @@
 import React from 'react';
+import data from './data';
 
 class Show extends React.Component {
+  componentWillMount() {
+    this.setState({
+      contact: data.filter(c => c.id === parseInt(this.props.params.contactID, 10))[0],
+    });
+  }
   render() {
     return (
       <div id='show'>
-        Show Contact
+        Id: {this.state.contact.id} <br/>
+        Name: {this.state.contact.name} <br/>
+        Email: {this.state.contact.email}
       </div>
     );
   }
